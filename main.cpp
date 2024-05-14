49d9f0e84703568401b2b89792453a045ebcb451
RaKalik963 <andrejmahot646@gmail.com>
2024-05-14 17:22:00 +0300

    lab-ex1

--- main.cpp
+++ main.cpp
@@ -2,8 +2,6 @@
 #include <cmath>
 #include <ctime>
 #include <chrono>
-#include <algorithm>
-#include <vector>
 using namespace std::chrono;
 using namespace std;
 
@@ -12,17 +10,15 @@ int main()
     srand(time(0));
     int num = rand() % 500;
     cout << num << endl;
+    auto start = high_resolution_clock::now();
     cout << "enter num to guess";
     int guess;
     cin >> guess;
-    auto start = high_resolution_clock::now();
-    int atempt = 1;
     for (; guess <= 503;)
     {
         if (guess == num)
         {
             cout << "you guess num" << endl;
-            cout << "count of atempt = " << atempt << endl;
             auto stop = high_resolution_clock::now();
             auto duration = duration_cast<seconds>(stop - start);
             cout << "Time taken by function: "
@@ -35,12 +31,10 @@ int main()
             cout << "your num not equal guessed num" << endl;
             cout << "enter num to guess";
             cin >> guess;
-            atempt += 1;
         }
         if (guess == 0)
         {
             cout << "guessed num = " << num << endl;
-            cout << "count of atempt = " << atempt << endl;
             system("pause");
             return 0;
         }
