b464466ff21da232b67c154465ea36a42de7e326
RaKalik963 <andrejmahot646@gmail.com>
2024-05-15 21:44:45 +0300

    23123213

--- main.cpp
+++ main.cpp
@@ -5,7 +5,24 @@ using namespace std;
 
 int main()
 {
+    cout << "enter two nums ";
+    int num1, num2;
+    cin >> num1 >> num2;
 
+    int min_num = (num1 < num2) ? num1 : num2;
+    min_num = (min_num < 0) ? -min_num : min_num;
+
+    int max_num = (num1 > num2) ? num1 : num2;
+    max_num = (max_num < 0) ? -max_num : max_num;
+
+    cout << "numbers are divisible without remainder. ";
+    for (int i = 1; i <= min_num; ++i) {
+        if (num1 % i == 0 && num2 % i == 0) {
+            std::cout << i << " ";
+        }
+    }
+
+    cout << endl;
     system("pause");
     return 0;
 }
