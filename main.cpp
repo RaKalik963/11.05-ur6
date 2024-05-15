a51ce5c8b25838c4fbd2fd278db40c30f500a223
RaKalik963 <andrejmahot646@gmail.com>
2024-05-15 21:29:06 +0300

    2321323

--- main.cpp
+++ main.cpp
@@ -5,7 +5,17 @@ using namespace std;
 
 int main()
 {
+    cout << "enter num ";
+    int A;
+    cin >> A;
 
+    for (int B = 1; B * B <= A; ++B) {
+        if (A % (B * B) == 0 && A % (B * B * B) != 0) {
+            cout << B << " ";
+        }
+    }
+
+    cout << endl;
     system("pause");
     return 0;
 }
