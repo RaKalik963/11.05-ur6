24256df5547811fdfaab3bbfe1bcd72887cb1f26
RaKalik963 <andrejmahot646@gmail.com>
2024-05-15 21:33:16 +0300

    3424234

--- main.cpp
+++ main.cpp
@@ -5,7 +5,18 @@ using namespace std;
 
 int main()
 {
+    cout << "enter num ";
+    int num;
+    cin >> num;
 
+    cout << "numbers that are divisible without remainder " << num;
+    for (int i = 1; i <= (num < 0 ? -num : num); ++i) {
+        if (num % i == 0) {
+            cout << i << " ";
+        }
+    }
+
+    cout << endl;
     system("pause");
     return 0;
 }
