4d5b07e1badf6c9fe878378b28835e8f778fd4a6
RaKalik963 <andrejmahot646@gmail.com>
2024-05-15 21:21:59 +0300

    dz-ex3

--- main.cpp
+++ main.cpp
@@ -5,7 +5,18 @@ using namespace std;
 
 int main()
 {
+    cout << "enter num ";
+    string number;
+    cin >> number;
 
+    string result;
+    for (int i = 0; i < number.size(); ++i) {
+        if (number[i] != '3' && number[i] != '6') {
+            result += number[i];
+        }
+    }
+
+    cout << "result " << result << endl;
     system("pause");
     return 0;
 }
