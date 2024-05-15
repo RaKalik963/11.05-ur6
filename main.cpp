34ef96d92f44966ea77462c98ca14bb4e8ed70ec
RaKalik963 <andrejmahot646@gmail.com>
2024-05-15 19:39:39 +0300

    dz-ex111111

--- main.cpp
+++ main.cpp
@@ -5,7 +5,19 @@ using namespace std;
 
 int main()
 {
+    int count = 0;
 
+    for (int i = 100; i <= 999; ++i) {
+        int hund = i / 100;
+        int ten = (i / 10) % 10;
+        int one = i % 10;
+
+        if (hund == ten || hund == one || ten == one) {
+            ++count;
+        }
+    }
+
+    cout << "count of numbers with similar num " << count << endl;
     system("pause");
     return 0;
 }
