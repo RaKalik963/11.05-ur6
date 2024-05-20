ea2ed047ec390460ba589537070bf39e5e66083f
RaKalik963 <andrejmahot646@gmail.com>
2024-05-20 17:47:10 +0300

    dz-x3

--- main.cpp
+++ main.cpp
@@ -5,18 +5,22 @@ using namespace std;
 
 int main()
 {
+    int num;
     cout << "enter num ";
-    string number;
-    cin >> number;
+    cin >> num;
 
-    string result;
-    for (int i = 0; i < number.size(); ++i) {
-        if (number[i] != '3' && number[i] != '6') {
-            result += number[i];
+    int result = 0;
+    int multiplier = 1;
+
+    for (int temp = num; temp != 0; temp /= 10) {
+        int digit = temp % 10;
+        if (digit != 3 && digit != 6) {
+            result += digit * multiplier;
+            multiplier *= 10;
         }
     }
 
-    cout << "result " << result << endl;
+    cout << "number without 3 and 6 ==== " << result << endl;
     system("pause");
     return 0;
 }
